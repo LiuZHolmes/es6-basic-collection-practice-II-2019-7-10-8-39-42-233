e43a437d26f7f6d81b135462b93cff6b47611567
retr0 <395637649@qq.com>
2019-07-14 22:08:01 +0800

    feat:practice-4.js

--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+module.exports = (collectionA, objectB) => {
+  const transform = (x) => {
+    if (x.includes('-')) {
+      return x[0].repeat(parseInt(x.split('-')[1]));
+    }
+    else return x;
+  }
+  return [... new Set(collectionA.map(transform).join('').split(''))]
+  .map(x => ({key:x,count:collectionA.map(transform).join('').split('')
+  .filter(y => x === y).length}))
+  .map(x => objectB.value.includes(x.key) ? ({key:x.key,count:x.count- Math.floor(x.count / 3)}) : x)
+
 }
